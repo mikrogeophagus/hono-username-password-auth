a497d50e99af46a9428e7cccb5298f49f48591f9
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2025-02-28 00:27:17 +0900

    React 向けのルールを削除

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import globals from "globals"
 import pluginJs from "@eslint/js"
 import tseslint from "typescript-eslint"
-import pluginReact from "eslint-plugin-react"
 import eslintConfigPrettier from "eslint-config-prettier"
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -10,6 +9,5 @@ export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
 ]
