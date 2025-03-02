72f51c706d1c5a92c2d07493543976523f1b571e
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2025-03-02 18:27:46 +0900

    CSRF に関するミドルウェアを設定

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { serve } from "@hono/node-server"
 import { Hono } from "hono"
+import { csrf } from "hono/csrf"
+import { secureHeaders } from "hono/secure-headers"
 
 import { indexRouter } from "./routes/index.js"
 
 const app = new Hono()
+
+app.use(csrf())
+app.use(secureHeaders({ referrerPolicy: "strict-origin-when-cross-origin" }))
 
 app.route("/", indexRouter)
 
