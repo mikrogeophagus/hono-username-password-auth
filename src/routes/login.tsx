dbd3bfe36c65d4c81cdb9a7a5edc8580fe15370f
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2025-03-04 19:52:12 +0900

    ログイン機能を実装

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,5 +1,14 @@
 import { Hono } from "hono"
+import { zValidator } from "@hono/zod-validator"
+import { z } from "zod"
+import { verify } from "@node-rs/argon2"
 import { Login } from "../views/Login.js"
+import { prisma } from "../lib/db.js"
+import {
+  createSession,
+  generateSessionToken,
+  setSessionTokenCookie,
+} from "../lib/session.js"
 import type { Context } from "../lib/context.js"
 
 export const loginRouter = new Hono<Context>()
@@ -16,3 +25,75 @@ loginRouter.get("/login", async (c) => {
 
   return c.html(<Login />, 200)
 })
+
+/**
+ * POST /login
+ */
+loginRouter.post(
+  "/login",
+  zValidator(
+    "form",
+    z.object({
+      email: z
+        .string()
+        .email({ message: "有効なメールアドレスを入力してください" }),
+      password: z
+        .string()
+        .min(8, { message: "8 文字以上のパスワードを入力してください" })
+        .max(100, { message: "100 文字以内のパスワードを入力してください" }),
+    }),
+    (result, c) => {
+      if (!result.success) {
+        return c.html(
+          <Login
+            values={result.data}
+            errors={result.error.flatten().fieldErrors}
+          />,
+          200,
+        )
+      }
+    },
+  ),
+  async (c) => {
+    const { email, password } = c.req.valid("form")
+
+    const existingUser = await prisma.user.findUnique({ where: { email } })
+
+    if (!existingUser) {
+      return c.html(
+        <Login
+          values={{ email }}
+          errors={{
+            email: ["メールアドレスまたはパスワードが正しくありません"],
+          }}
+        />,
+        200,
+      )
+    }
+
+    const validPassword = await verify(existingUser.password, password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    })
+
+    if (!validPassword) {
+      return c.html(
+        <Login
+          values={{ email }}
+          errors={{
+            password: ["メールアドレスまたはパスワードが正しくありません"],
+          }}
+        />,
+        200,
+      )
+    }
+
+    const token = generateSessionToken()
+    const session = await createSession(token, existingUser.id)
+    setSessionTokenCookie(c, token, session.expiresAt)
+
+    return c.redirect("/")
+  },
+)
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,17 +1,45 @@
 import { Layout } from "./Layout.js"
 
-export function Login() {
+interface LoginProps {
+  values?: {
+    email?: string
+    password?: string
+  }
+
+  errors?: {
+    email?: string[]
+    password?: string[]
+  }
+}
+
+export function Login({ values, errors }: LoginProps) {
   return (
     <Layout title="ログイン">
       <h1>ログイン</h1>
       <form action="/login" method="post">
         <div>
           <label htmlFor="email">メールアドレス</label>
-          <input type="email" name="email" id="email" required />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={values?.email}
+            autocomplete={"email"}
+            required
+          />
+          <div>{errors?.email?.map((error) => <div>{error}</div>)}</div>
         </div>
         <div>
           <label htmlFor="password">パスワード</label>
-          <input type="password" name="password" id="password" required />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={values?.password}
+            autocomplete={"current-password"}
+            required
+          />
+          <div>{errors?.password?.map((error) => <div>{error}</div>)}</div>
         </div>
         <button type="submit">ログイン</button>
       </form>
