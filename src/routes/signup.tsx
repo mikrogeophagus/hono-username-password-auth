d9cc418c1e6b07ac9512244bfcedc15d35eba800
Kaisei Yoneyama <96284126+Kaisei-Yoneyama@users.noreply.github.com>
2025-03-08 02:59:09 +0900

    略記プロパティ名を使用

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -77,7 +77,7 @@ signupRouter.post(
       const user = await prisma.user.create({
         data: {
           email,
-          passwordHash: passwordHash,
+          passwordHash,
         },
       })
 
